5b05bdce6af3fb86ed18c71341ae81c3f821b0b7
Rakhikale04 <rakhikale04@gmail.com>
2023-09-10 12:40:09 +0530

    Final commit

---
+++ Addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	clrscr();
+	int num1=0, num2=0, sum=0;
+	printf("Enter two integers:");
+	scanf("%d %d", &num1, &num2);
+	
+	sum=num1+num2
+	getch();
+}
