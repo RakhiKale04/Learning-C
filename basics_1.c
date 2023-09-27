b06d4e239f6154771e2110cee5d288bade702379
Rakhi Kale <rakhikale04@gmail.com>
2023-09-27 15:21:59 +0530

    final commit

---
+++ basics_1.c
@@ -0,0 +1,10 @@
+//C code for printing name, date of birth and phone number
+
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+	printf("My name: Rakhi Dilip Kale");
+	printf("MY date of birth: 29/08/2004");
+	printf("My phone number: 99-99999999");
+}
